f5c0fbb4be214190b0c02bdd21550d574fee1496
Pakbung Nathchanon <35398117+pakbungnatchanon@users.noreply.github.com>
2020-10-12 22:30:53 +0700

    edit add category

--- routes/addproductController.js
+++ routes/addproductController.js
@@ -1,11 +1,9 @@
 var express = require('express');
-
+const db = require('../model/db');
 var router = express.Router();
 
 
-router.get('/', async function (req, res, next) {
-   res.render('addproduct');
-});
+
 
 router.get("/", async (req, res) => {
    const storeId = req.params.storeId;
@@ -25,10 +23,10 @@ router.get("/", async (req, res) => {
          menuList = menuList.filter((item) => item.category == categoryId);
    }
    console.log(storeId);
-   console.log(shopName);
+   console.log('name' + shopName);
    console.log(menuList);
-   console.log(categoriesList);
-   console.log(categoriesFilter);
+   console.log('cat ' + categoriesList);
+   console.log('list' + categoriesFilter);
  
    res.render("addproduct", { storeId, shopName, menuList, categoriesList, categoriesFilter });
  
