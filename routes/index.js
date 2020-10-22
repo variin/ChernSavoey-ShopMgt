d9ecf0c4a9c801a2db790443ddb7a510dc17cd5d
variin <35445635+variin@users.noreply.github.com>
2020-10-22 20:18:55 +0700

    fluke commit

--- routes/index.js
+++ routes/index.js
@@ -28,11 +28,11 @@ router.get("/", async (req, res) => {
             categoriesFilter = menuDetails.categories.filter((item) => item.category == categoryId)
             menuList = menuList.filter((item) => item.category == categoryId);
       }
-      console.log(storeId);
-      console.log(shopName);
-      console.log(menuList);
-      console.log(categoriesList);
-      console.log(categoriesFilter);
+      // console.log(storeId);
+      // console.log(shopName);
+      // console.log(menuList);
+      // console.log(categoriesList);
+      // console.log(categoriesFilter);
 
       res.render("index", { storeId, shopName, menuList, categoriesList, categoriesFilter });
 
@@ -56,52 +56,71 @@ router.get("/:categoryId", async (req, res) => {
             categoriesFilter = menuDetails.categories.filter((item) => item.category == categoryId)
             menuList = menuList.filter((item) => item.category == categoryId);
       }
-      console.log(storeId);
-      console.log(shopName);
-      console.log(menuList);
-      console.log(categoriesList);
-      console.log(categoriesFilter);
+      // console.log(storeId);
+      // console.log(shopName);
+      // console.log(menuList);
+      // console.log(categoriesList);
+      // console.log(categoriesFilter);
 
 
       res.render("index", { storeId, shopName, menuList, categoriesList, categoriesFilter, });
 }
 );
 
-router.get("/deleteMenu/:categoryId/:menuId", async function (req, res, next) {
-      // const storeId = req.params.storeId;
-      const categoryId = req.params.categoryId;
+router.post("/:categoryId/:menuId/deleteMenu", async function (req, res, next) {
+      // const categoryId = req.params.categoryId;
       const menuId = req.params.menuId;
 
-      // console.log(storeId);
-      console.log(categoryId);
-      console.log(menuId);
-      const storeRef = await db.collection("store");
-      await storeRef.doc("cafeAmazon").get().then((querySnapshot) => querySnapshot.data());
-      const shopName = storeRef.storeName;
-      let cartList = storeRef.menu;
+      let storeList = [];
+      const menuDetails = await db.collection("store").doc("cafeAmazon").get()
+            .then((querySnapshot) => 
+                  querySnapshot.data()
+                  // storeList.push(querySnapshot.data())
+            );
+
+      let menuList = menuDetails.menu;
+      console.log('menuList: ', menuList);
+
       if (menuId) {
-            cartList = cartList.filter((item) => item.menuId == menuId);
+          let  menuList_remove = menuList.filter((item) => item.menuId == menuId);
+            console.log('menuList_remove: ', menuList_remove);
       }
 
-      console.log(shopName);
-      console.log(cartList);
+      // --> เอา menuList_remove ลบออกจาก menuList
+      // --> new menuList
+      // --> update 
 
-      await storeRef.update({
-            "menu": await firebase.firestore.FieldValue.arrayRemove
-                  ({ "category": categoryId, 
-                  "menuId": menuId, 
-                  "menuImg": menuImg, 
-                  "menuName": menuName, 
-                  "price": price })
-      })
-            .then(function (db) {
-                  console.log("Document written with ID: ", db.id);
-            })
-            .catch(function (error) {
-                  console.error("Error adding document: ", error);
-            });
-      res.redirect("/")
 
+
+
+      
+
+      // let store_menu_map = storeList.map(m => m.menu);
+      // console.log('store_menu_map: ', store_menu_map);
+
+
+
+      // console.log(shopName);
+      // console.log('cart list: ', cartList);
+
+      // await docRef.update({
+      //       "menu": db.FieldValue.arrayRemove({
+      //             "category": categoryId,
+      //             "menuId": menuId,
+      //             "menuImg": menuImg,
+      //             "menuName": menuName,
+      //             "price": price,
+      //             "qty": qty
+      //       })
+      // })
+      //       .then(function (db) {
+      //             console.log("Document written with ID: ", db.id);
+      //       })
+      //       .catch(function (error) {
+      //             console.error("Error adding document: ", error);
+      //       });
+
+      res.redirect("/");
 
       // const storeRef = db.collection('store').doc("cafeAmazon");
       // await storeRef.update({
